afa6a18368abcb1e3193655ef06f09998a8084e5
=ddelvill <dayanasdelvillar@gmail.com>
2023-04-02 20:31:15 +0200

    Devuelve cliente dando Id y su hotel.

--- src/main/java/com/example/controllers/ClienteController.java
+++ src/main/java/com/example/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package com.example.controllers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,4 +81,49 @@ public class ClienteController {
 
     return responseEntity;
     }    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> findById(@PathVariable(name = "id") Integer id) {
+
+        ResponseEntity<Map<String, Object>> responseEntity = null;
+
+        Map<String, Object> responseAsMap = new HashMap<>();
+
+
+
+
+        try {
+
+            //Producto producto = productoService.findById(id);
+
+            Cliente cliente = clienteService.findById(id);
+
+            if (cliente != null) {
+            String successMessage = "Se ha encontrado el cliente con id: " + id + " correctamente";
+            responseAsMap.put("mensaje", successMessage);
+            responseAsMap.put("cliente", cliente);
+            responseEntity = new ResponseEntity<Map<String, Object>>(responseAsMap, HttpStatus.OK);
+            
+        } else {
+
+            String errorMessage = "No se ha encontrado el cliente con id: " + id;
+            responseAsMap.put("error", errorMessage);
+            responseEntity = new ResponseEntity<Map<String,Object>>(responseAsMap, HttpStatus.NOT_FOUND);
+
+        }
+
+        } catch (Exception e) {
+
+            String errorGrave = "Error grave";
+            responseAsMap.put("error", errorGrave);
+            responseEntity = new ResponseEntity<Map<String,Object>>(responseAsMap, HttpStatus.INTERNAL_SERVER_ERROR);
+
+          
+        }
+
+
+
+        return responseEntity;
+    }
+
 }
