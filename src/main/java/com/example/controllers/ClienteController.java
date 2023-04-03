90c104a9f8ee7acba427c64eddf7e448676c100a
=ddelvill <dayanasdelvillar@gmail.com>
2023-04-03 23:44:44 +0200

    TERMINADO

--- src/main/java/com/example/controllers/ClienteController.java
+++ src/main/java/com/example/controllers/ClienteController.java
@@ -7,16 +7,20 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -356,6 +360,87 @@ public class ClienteController {
 
         return responseEntity;
     }
+
+
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<String> delete(@Valid   @PathVariable(name = "id") Integer id) {
+
+
+        ResponseEntity<String> responseEntity = null;
+        try {
+           
+
+            Cliente clienteDelete = clienteService.findById(id);
+
+            
+
+
+            if (clienteDelete != null) {
+
+                clienteService.delete(clienteDelete);
+
+                String mensajeOk = "Se ha borrado correctamente.";
+
+                responseEntity = new ResponseEntity<String>(mensajeOk, HttpStatus.OK);
+
+                              
+
+
+
+            } else {
+
+                String mensajeError = "No existe el cliente que quiere borrar.";
+
+                responseEntity = new ResponseEntity<String>(mensajeError, HttpStatus.NOT_FOUND);
+            }
+
+    
+            
+           } catch (DataAccessException e) {
+            e.getMostSpecificCause();
+              responseEntity = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+            
+             }
+
+
+       
+    return responseEntity;
+
+
+
+        
+
+
+    }
+
+
+    @GetMapping("/downloadFile/{fileCode}")
+    public ResponseEntity<?> downloadFile(@PathVariable(name = "fileCode") String fileCode) {
+
+        Resource resource = null;
+
+        try {
+            resource = fileDownloadUtil.getFileAsResource(fileCode);
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+        if (resource == null) {
+            return new ResponseEntity<>("File not found ", HttpStatus.NOT_FOUND);
+        }
+
+        String contentType = "application/octet-stream";
+        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+
+        return ResponseEntity.ok()
+        .contentType(MediaType.parseMediaType(contentType))
+        .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+        .body(resource);
+
+    }
+
 
 
    
